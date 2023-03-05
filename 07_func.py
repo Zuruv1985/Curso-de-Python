4643ca1deac4c11c1c6f511f93cd116e215a5caf
Carlos <cnavarro_zuru@hotmail.com>
2023-03-05 10:59:39 -0600

    Upload file

---
+++ 07_func.py
@@ -0,0 +1,22 @@
+print("Hola")
+
+def my_print():
+    print ("this is my print")
+    print("This is my print 2")
+
+my_print()
+
+def my_print2(text):
+    print (text*2)
+    
+
+my_print2("ESte es mi texto")
+my_print2("hola")
+
+def sum (a,b):
+    return (a+b)
+
+result= sum(5,3)
+result1=sum(10,4)
+print (result)
+print(result1)
