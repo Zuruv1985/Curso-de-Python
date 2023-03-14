f8738f771ba290dbbb3dad4835325e017a00f329
Carlos <cnavarro_zuru@hotmail.com>
2023-03-14 09:52:17 -0600

    new files

--- 12_Lambda.py
+++ 12_Lambda.py
@@ -16,6 +16,17 @@ increment_v2=lambda x:x+1
 print (increment_v2)
 
 full_name= lambda name, last_name:f"Full name is {name.title()} {last_name.title()}"
+def increment(x):
+    return x+1
+result=increment(10)
+print(result)
 
+increment_v2=lambda x:x+1
+print (increment_v2)
+
+full_name= lambda name, last_name:f"Full name is {name.title()} {last_name.title()}"
+
+text=full_name("Carlos", "Navarro")
+print (text)
 text=full_name("Carlos", "Navarro")
 print (text)
--- 15_map_dics.py
+++ 15_map_dics.py
@@ -1,3 +1,15 @@
+item = [{"product":"Camisa", "price":100},
+    {"product":"pantalon", "price":300},
+{"product":"pantalon_1", "price":200,}]
+
+prices=list(map(lambda item:item["price"], item))
+print (prices)
+products=list(map(lambda items:items["product"], item))
+print (products)
+
+def add_taxes(item):
+    item["taxes"]= item["price"]*0.19
+    return item
 <<<<<<< HEAD
 item = [{"product":"Camisa", "price":100},
     {"product":"pantalon", "price":300},
@@ -28,5 +40,4 @@ def add_taxes(item):
     return item
 
 new_items=list(map(add_taxes, item))
->>>>>>> 2f9545c9c2433d20a64f45b71c5f66c3d994ca24
 print (new_items)
